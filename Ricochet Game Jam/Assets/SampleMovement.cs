7e2dde2b6ea5958c6440da227ec6728d3755ce4e
Julianna <Julianna@LAPTOP-OJC01Q1O>
2018-09-01 13:18:37 -0400

    added a sample enemy that navigates to the player

---
+++ Ricochet Game Jam/Assets/SampleMovement.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.AI;
+
+//this is a sample movement script
+public class SampleMovement : MonoBehaviour
+{
+    public Transform goal;
+    void Start()
+    {
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.destination = goal.position;
+    }
+}
