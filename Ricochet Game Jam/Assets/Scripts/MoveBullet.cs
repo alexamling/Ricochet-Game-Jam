b1a9be2c9f15fd2b776bdde10b20e0667b482e5d
Alexander Amling <aja7073@rit.edu>
2018-09-01 15:06:41 -0400

    Fixed merge issues by transfering the proper files

---
+++ Ricochet Game Jam/Assets/Scripts/MoveBullet.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBullet : MonoBehaviour {
+
+    //Variables
+    private float speed;
+    private Vector2 trajectory;
+    private Vector2 velocity;
+
+    //Properties
+    public float Speed
+    {
+        get
+        {
+            return speed;
+        }
+    }
+
+    //Default Methods
+	void Start () {
+		
+	}
+	
+	void Update () {
+
+        velocity.x = trajectory.x * speed;
+        velocity.y = trajectory.y * speed;
+
+
+        transform.position = new Vector3(velocity.x + transform.position.x, transform.position.y, velocity.y + transform.position.z);
+    }
+
+    //Special Methods
+    public void NewMove(float speed, Vector2 trajectory)
+    {
+        this.speed = speed;
+        this.trajectory = trajectory.normalized;
+    }
+}
---
+++ Ricochet Game Jam/Assets/Scripts/Movement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    //Inspector variables
+    [SerializeField]
+    private float walkSpeed;
+    [SerializeField]
+    private float runSpeed;
+
+    //Other variables
+    private Vector2 velocity;
+    private float currentSpeed;
+    private Vector3 newPosition;
+    
+    //Default Methods
+	void Start () {
+		
+	}
+	
+	void Update () {
+        currentSpeed = walkSpeed;
+
+        if (Input.GetAxisRaw("Fire3") > 0)
+        {
+            currentSpeed = runSpeed;
+        }
+
+        velocity.x = Input.GetAxisRaw("Horizontal") * currentSpeed;
+        velocity.y = Input.GetAxisRaw("Vertical") * currentSpeed;
+
+
+        transform.position = new Vector3 (velocity.x + transform.position.x, transform.position.y, velocity.y + transform.position.z);
+    }
+}
